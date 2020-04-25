121e2d5d3755d3db1b26af2dfdb90ece6a7030f2
Billy <billy.meas@yahoo.com>
2020-04-24 22:25:04 -0500

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@
 #  name              :string
 #  password_digest   :string
 #  remember_digest   :string
+#  reset_digest      :string
+#  reset_sent_at     :datetime
 #  created_at        :datetime         not null
 #  updated_at        :datetime         not null
 #
@@ -19,7 +21,7 @@
 #  index_users_on_email  (email) UNIQUE
 #
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
   before_create :create_activation_digest
@@ -54,7 +56,7 @@ class User < ApplicationRecord
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest") #send is a Rails method. It lets us call a method with a name of choice by sending a message to a given object.
     return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
+    BCrypt::Password.new(digest).is_password?(token) #BCrypt::Password.new(digest) will automatically decrypt the digest and compares to the token.
   end
 
   # Forgets a user.
@@ -65,13 +67,27 @@ class User < ApplicationRecord
   # Activates an account.
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
-    # update_attribute(:activated, true)
-    # update_attribute(:activated_at, Time.zone.now)
   end
 
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
